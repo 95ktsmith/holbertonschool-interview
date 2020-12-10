938d2d32119df7e44f1d43b7d2e76f401aaa2b30
95ktsmith <95ktsmith@gmail.com>
2020-12-10 01:36:02 +0000

    Initial commit.

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Lockboxes """
+
+
+def canUnlockAll(boxes):
+    """ Will return True if all boxes can be unlocked, or False otherwise """
+    return False not in unlock(0, boxes, [False for i in range(0, len(boxes))])
+
+
+def unlock(box, boxes, unlocked):
+    """ If not already unlocked, unlocks the current box and unlocks the boxes
+        with the keys it contains.
+    """
+    if unlocked[box] == False:
+        unlocked[box] = True
+        for key in boxes[box]:
+            unlock(key, boxes, unlocked)
+    return unlocked
