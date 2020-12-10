b671548093ff69a614a7ee61d6a93cc4a573d037
95ktsmith <95ktsmith@gmail.com>
2020-12-10 01:47:41 +0000

    Added check if there are no boxes.

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -6,6 +6,8 @@ def canUnlockAll(boxes):
     """ Will return True if all boxes can be unlocked, or False otherwise """
     if boxes is None:
         return False
+    if len(boxes) == 0:
+        return True
     return False not in unlock(0, boxes, [False for i in range(0, len(boxes))])
 
 
@@ -13,7 +15,7 @@ def unlock(box, boxes, unlocked):
     """ If not already unlocked, unlocks the current box and unlocks the boxes
         with the keys it contains.
     """
-    if unlocked[box] == False:
+    if unlocked[box] is False:
         unlocked[box] = True
         for key in boxes[box]:
             if key < len(boxes):
