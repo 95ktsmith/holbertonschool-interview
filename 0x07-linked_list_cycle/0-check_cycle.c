56491c00f5eebaa69e5bc297d476c3e078077a05
95ktsmith <95ktsmith@gmail.com>
2021-02-04 23:59:50 +0000

    Initial commit.

---
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * check_cycle - check a link list for a cycle
+ * @list: Pointer to the list to check
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *slow, *fast;
+
+	if (list == NULL)
+		return (0);
+
+	slow = list;
+	fast = list->next;
+	while (fast->next != NULL && fast->next->next != NULL)
+	{
+		if (slow == fast)
+			return (1);
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+
+	return (0);
+}
