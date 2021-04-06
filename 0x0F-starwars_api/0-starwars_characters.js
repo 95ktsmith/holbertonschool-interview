025a04dac9fd871a89f0fabe5271e9566fdaf7a0
95ktsmith <95ktsmith@gmail.com>
2021-04-06 10:58:20 -0400

    Initial commit.

---
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv.slice(2)[0];
+request({ url: url, method: 'GET' },
+  function (error, response, body) {
+    if (error) {
+      console.log(error);
+    } else {
+      const info = JSON.parse(body);
+      for (let i = 0; i < info.characters.length; i++) {
+        request({ url: info.characters[i], method: 'GET' },
+          function (charError, charResponse, charBody) {
+            if (charError) {
+              console.log(charError);
+            } else {
+              const charInfo = JSON.parse(charBody);
+              console.log(charInfo.name);
+            }
+          });
+      }
+    }
+  });
