4d7de64da2869f9126e7ecdaaa815ed240810e77
95ktsmith <95ktsmith@gmail.com>
2021-05-06 17:49:51 -0400

    Made to search all pages in hot section instead of just the front page.

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -3,37 +3,19 @@
 import requests
 
 
-def count_words(subreddit, word_list=[], posts=[], count={}):
+def count_words(subreddit, word_list=[], count={}, after=""):
     """
     Prints the number of times each word in word_list appears in the titles
     of posts on the front page of subreddit.
     Words with no matches are omitted.
     """
 
-    # If no more words to look for, print counts and return
-    if len(word_list) == 0:
-        count = sorted(count.items(), key=lambda x: (-x[1], x[0]))
-        for item in count:
-            if item[1] != 0:
-                print("{}: {}".format(item[0], item[1]))
-        return
+    # If default after (first function call), prep word list and count
+    # dictionary
+    if after == "":
+        url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
 
-    # If no posts (probably first function call), make api request
-    if len(posts) == 0:
-        r = requests.get(
-            "https://www.reddit.com/r/{}/hot.json".format(subreddit),
-            allow_redirects=False,
-            headers={'User-Agent': 'PlaceholderAgent'}
-        )
-        if r.status_code != 200:
-            return
-
-        # Make everything lower case because case insensitive
-        posts = list(map(
-            lambda x: x['data']['title'].lower(),
-            r.json()['data']['children']
-        ))
-
+        # Make list of lowercase search words
         word_list = list(map(
             lambda x: x.lower(),
             word_list
@@ -42,12 +24,40 @@ def count_words(subreddit, word_list=[], posts=[], count={}):
         # Build dictionary of keyword: count
         for word in word_list:
             count[word] = 0
+    else:
+        url = "https://www.reddit.com/r/{}/hot.json?after={}".\
+              format(subreddit, after)
+
+    r = requests.get(
+        url,
+        allow_redirects=False,
+        headers={'User-Agent': 'PlaceholderAgent'}
+    )
+
+    # Stop if request didn't work
+    if r.status_code != 200:
+        return
+
+    # Make list of lowercase titles
+    posts = list(map(
+        lambda x: x['data']['title'].lower(),
+        r.json()['data']['children']
+    ))
 
     # Count up for each matching word in each post title
     for title in posts:
         for word in title.split():
-            if word == word_list[0]:
-                count[word] += 1
+            for keyword in word_list:
+                if word == keyword:
+                    count[word] += 1
 
-    count_words(subreddit, word_list[1:], posts, count)
-    return None
+    # Print results and stop after searching last page
+    if r.json()['data']['after'] is None:
+        count = sorted(count.items(), key=lambda x: (-x[1], x[0]))
+        for item in count:
+            if item[1] != 0:
+                print("{}: {}".format(item[0], item[1]))
+        return
+
+    # Count words on the next page
+    count_words(subreddit, word_list, count, r.json()['data']['after'])
