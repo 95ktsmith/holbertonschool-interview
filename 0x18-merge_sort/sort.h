b8c67dff4b6b74780d1cd8a28e2dc53d027fc686
95ktsmith <95ktsmith@gmail.com>
2021-06-09 10:58:31 -0400

    Added print_array prototype.

--- 0x18-merge_sort/sort.h
+++ 0x18-merge_sort/sort.h
@@ -3,6 +3,7 @@
 
 #include <stddef.h>
 
+void print_array(const int *array, size_t size);
 void merge_sort(int *array, size_t size);
 void merge(int *left, size_t left_s, int *right, size_t right_s);
 
