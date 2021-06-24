86a2fc84e3ceb5d48ac8ee85cd826cfeb781b332
95ktsmith <95ktsmith@gmail.com>
2021-06-24 18:39:56 -0400

    Initial commit.

---
+++ 0x19-making_change/0-making_change.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+""" Making change """
+
+
+def makeChange(coins, total):
+    """
+    Determines the fewest number of coins needed to meet a given amount total
+    coins: List of the values of the coins in your possession
+    total: The total value to meet
+    Returns the fewest number of coins
+    """
+    coins = sorted(coins, reverse=True)
+    coinCount = 0
+
+    for coin in coins:
+        coinCount += int(total / coin)
+        total = total % coin
+
+    if total != 0:
+        return -1
+
+    return coinCount
