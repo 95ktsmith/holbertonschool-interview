ee42f5dd27eaacd8299ad241c8710ba794f73c64
95ktsmith <95ktsmith@gmail.com>
2021-08-11 12:26:42 -0400

    Clean recursive version

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -14,18 +14,12 @@ def minOperations(n):
     Returns the number of operations needed to reach n number of H characters
     Returns 0 if n is impossible to achieve (n is less than 1)
     """
-    operations = 0
-    while n > 1:
-        # Find largest factor
-        for number in range(int(n / 2), 0, -1):
-            if n % number == 0:
-                largest_factor = number
-                break
-        # Add a number of operations equal to n divided by its largest factor
-        operations += n / largest_factor
+    if n <= 1:
+        return 0
 
-        # Set n equal to this largest_factor
-        n = largest_factor
-        # Loop to find number of operations required to get to this factor
-
-    return int(operations)
+    # Find n's largest factor
+    for number in range(int(n / 2), 0, -1):
+        if n % number == 0:
+            # Return a number of operations equal to n divided by its largest
+            # factor, and the number of operations needed to reach that factor
+            return int(n / number) + minOperations(number)
