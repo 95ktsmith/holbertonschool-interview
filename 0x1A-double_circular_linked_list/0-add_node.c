a311b232e0bcce2378edcec1fa10ea1f55f48d50
95ktsmith <95ktsmith@gmail.com>
2021-08-30 19:45:53 -0400

    Initial commit.

---
+++ 0x1A-double_circular_linked_list/0-add_node.c
@@ -0,0 +1,74 @@
+#include "list.h"
+#include <stdlib.h>
+
+
+/**
+ * add_node_end - Adds a node to the end of the list
+ * @list: Pointer to the address of the head of the list
+ * @str: String to copy into the new node
+ * Return: Address of the new node, or NULL on failure.
+ */
+List *add_node_end(List **list, char *str)
+{
+	List *node;
+
+	if (list == NULL)
+		return (NULL);
+
+	node = malloc(sizeof(List));
+	if (node == NULL)
+		return (NULL);
+	node->str = str;
+
+	if (*list == NULL)
+	{
+		*list = node;
+		node->next = node;
+		node->prev = node;
+	}
+	else
+	{
+		node->next = *list;
+		node->prev = (*list)->prev;
+		(*list)->prev->next = node;
+		(*list)->prev = node;
+	}
+
+	return (node);
+}
+
+/**
+ * add_node_begin - Adds a node to the beginning of the list
+ * @list: Pointer to the address of the head of the list
+ * @str: String to copy into the new node
+ * Return: Address of the new node, or NULL on failure.
+ */
+List *add_node_begin(List **list, char *str)
+{
+	List *node;
+
+	if (list == NULL)
+		return (NULL);
+
+	node = malloc(sizeof(List));
+	if (node == NULL)
+		return (NULL);
+	node->str = str;
+
+	if (*list == NULL)
+	{
+		node->next = node;
+		node->prev = node;
+	}
+	else
+	{
+		node->next = *list;
+		node->prev = (*list)->prev;
+		(*list)->prev->next = node;
+		(*list)->prev = node;
+	}
+
+	*list = node;
+
+	return (node);
+}
---
+++ 0x1A-double_circular_linked_list/README.md
@@ -0,0 +1,1 @@
+# Insert into double circular linked list
